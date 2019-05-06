b1d82e7ad563a8948f29938f1f458c02088d5a27
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2019-05-06 09:40:20 +0200

    Fix timezone issues on linux

--- LocalClock.cs
+++ LocalClock.cs
@@ -4,7 +4,20 @@ namespace KajfestPOS
 {
     public static class LocalClock
     {
-        private static readonly TimeZoneInfo Timezone = TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time");
-        public static DateTimeOffset Now => TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, Timezone);
+        private static readonly TimeZoneInfo _timezone = TryGetTimezone("Romance Standard Time") ?? TryGetTimezone("Europe/Copenhagen") ?? throw new TimeZoneNotFoundException("Unable to get timezone");
+
+        public static DateTimeOffset Now => TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, _timezone);
+
+        private static TimeZoneInfo TryGetTimezone(string id)
+        {
+            try
+            {
+                return TimeZoneInfo.FindSystemTimeZoneById(id);
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
