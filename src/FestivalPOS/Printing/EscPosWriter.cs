c3eb2936d3073ea919ec74578b101f8141de1fb4
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2019-06-13 22:10:03 +0200

    Add stub print generators and EscPos writer

---
+++ src/FestivalPOS/Printing/EscPosWriter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FestivalPOS.Printing
+{
+    public class EscPosWriter : IDisposable
+    {
+        private const byte ESC = 0x1b;
+        private const byte GS = 0x1d;
+        private const byte LF = 0x0a;
+        private readonly Stream _stream;
+
+        public int LineWidth = 42;
+
+        public EscPosWriter(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public void Initialize()
+        {
+            _stream.WriteByte(ESC);
+            _stream.WriteByte((byte)'@');
+        }
+
+        public void SetCodeTable(CodeTable value)
+        {
+            _stream.WriteByte(ESC);
+            _stream.WriteByte((byte)'t');
+            _stream.WriteByte((byte)value);
+        }
+
+        public void SetHorizontalTabPositions(params int[] positions)
+        {
+            _stream.WriteByte(ESC);
+            _stream.WriteByte((byte)'D');
+            foreach (var position in positions)
+            {
+                _stream.WriteByte((byte)position);
+            }
+            _stream.WriteByte(0);
+        }
+
+        public void SetFontSize(int width, int height)
+        {
+            _stream.WriteByte(ESC);
+            _stream.WriteByte((byte)'i');
+            _stream.WriteByte((byte)width);
+            _stream.WriteByte((byte)height);
+        }
+
+        public void SetEmphasize(bool value)
+        {
+            _stream.WriteByte(ESC);
+            _stream.WriteByte((byte)'E');
+            _stream.WriteByte((byte)(value ? 1 : 0));
+        }
+
+        public void Newline()
+        {
+            _stream.WriteByte(LF);
+        }
+
+        public void Text(string value)
+        {
+            var encoding = Encoding.GetEncoding(1252);
+            _stream.Write(encoding.GetBytes(value));
+        }
+
+        public void HorizontalTab()
+        {
+            _stream.WriteByte(0x09);
+        }
+
+        public void CutPaper(bool partialCut = false)
+        {
+            _stream.WriteByte(GS);
+            _stream.WriteByte((byte)'V');
+            _stream.WriteByte((byte)(partialCut ? 1 : 0));
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+    }
+
+    public enum CodeTable
+    {
+        Latin1_Windows1252 = 16,
+        Nordic_PC865 = 5,
+        Euro_PC858 = 19
+    }
+}
--- src/FestivalPOS/Printing/TicketPrintGenerator.cs
+++ src/FestivalPOS/Printing/TicketPrintGenerator.cs
@@ -1,9 +1,5 @@
 ﻿using FestivalPOS.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FestivalPOS.Printing
 {
@@ -11,7 +7,46 @@ namespace FestivalPOS.Printing
     {
         public byte[] Generate(Order order)
         {
-            return Encoding.ASCII.GetBytes("BILLET");
+            using (var stream = new MemoryStream())
+            using (var writer = new EscPosWriter(stream))
+            {
+                writer.Initialize();
+                writer.SetCodeTable(CodeTable.Latin1_Windows1252);
+
+                WriteHeader(writer, order);
+                WriteReceiveableLines(writer, order);
+
+                for (var i = 0; i < 5; i++)
+                {
+                    writer.Newline();
+                }
+
+                writer.CutPaper();
+
+                return stream.ToArray();
+            }
+        }
+
+        private static void WriteHeader(EscPosWriter writer, Order order)
+        {
+            writer.Text($"Billet {order.Id}");
+            writer.Newline();
+        }
+
+        private static void WriteReceiveableLines(EscPosWriter writer, Order order)
+        {
+            writer.SetHorizontalTabPositions(4);
+
+            foreach (var line in order.Lines)
+            {
+                if (line.Receiveable > 0)
+                {
+                    writer.Text(line.Receiveable.ToString());
+                    writer.HorizontalTab();
+                    writer.Text(line.Name);
+                    writer.Newline();
+                }
+            }
         }
     }
 }
