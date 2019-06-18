4bcd40466586e7171e150ab58df7aac351c101bb
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2019-06-18 09:36:05 +0200

    Add pos name to navigation

--- src/FestivalPOS/src/top-navbar.ts
+++ src/FestivalPOS/src/top-navbar.ts
@@ -1,8 +1,24 @@
+import { Api } from "./api";
 import { AppRouter } from "aurelia-router";
+import { State } from "./state";
 import { autoinject } from "aurelia-framework";
+import { connectTo } from "aurelia-store";
 
+@connectTo()
 @autoinject()
 export class TopNavbar {
-    constructor(protected router: AppRouter) {
+    private state!: State;
+    private pointOfSaleId?: number;
+    pointOfSaleName?: string;
+
+    constructor(private api: Api, protected router: AppRouter) {
+    }
+
+    protected async stateChanged(newState: State) {
+        if (newState.pointOfSaleId !== this.pointOfSaleId) {
+            this.pointOfSaleId = newState.pointOfSaleId;
+            const pos = await this.api.getPointOfSale(this.pointOfSaleId).transfer();
+            this.pointOfSaleName = pos.name;
+        }
     }
 }
