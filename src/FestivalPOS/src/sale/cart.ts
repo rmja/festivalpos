1490f18f1f2a83b97efb273cbee0ead87be79ace
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2019-07-10 23:30:07 +0200

    Navigate to products when deleting last order line or emptying cart

--- src/FestivalPOS/src/sale/cart.ts
+++ src/FestivalPOS/src/sale/cart.ts
@@ -1,7 +1,9 @@
-import { autoinject, computedFrom } from "aurelia-framework";
-import { connectTo, dispatchify } from "aurelia-store";
 import { State, removeOrderLine, resetOrder } from "../state";
+import { Store, connectTo } from "aurelia-store";
+import { autoinject, computedFrom } from "aurelia-framework";
+
 import { Big } from "big.js";
+import { Router } from "aurelia-router";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 @autoinject()
@@ -10,12 +12,25 @@ export class Cart {
     private state!: State;
     faShoppingCart = faShoppingCart;
 
-    delete = dispatchify(removeOrderLine);
-    clear = dispatchify(resetOrder);
+    constructor(private store: Store<State>, private router: Router) {
+    }
 
     @computedFrom("state")
     get orderLines(): OrderLineViewModel[] {
         return this.state.orderLines;
+    }
+
+    async delete(index: number) {
+        await this.store.dispatch(removeOrderLine, index);
+
+        if (this.state.orderLines.length === 0) {
+            this.router.navigateToRoute("products");
+        }
+    }
+
+    async clear() {
+        await this.store.dispatch(resetOrder);
+        this.router.navigateToRoute("products");
     }
 }
 
